f130de26bc71f4ead77745198ea272e66e636abd
Daniel Vartanov <dan@vartanov.net>
2012-10-07 08:30:05 +0600

    Empty Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BarcampKg2012AcceptanceTestsDemo::Application.config.secret_token = 'e3f59d60b53dfa0d5f48ba32d3c9a5bcac92c8782b2234694b461dc507f37c70e8c7ee0875fda2a9e272140dced30eeba52ae93aa72298094485450a9ba89ccf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BarcampKg2012AcceptanceTestsDemo::Application.config.session_store :cookie_store, key: '_barcamp-kg-2012-acceptance-tests-demo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BarcampKg2012AcceptanceTestsDemo::Application.config.session_store :active_record_store
