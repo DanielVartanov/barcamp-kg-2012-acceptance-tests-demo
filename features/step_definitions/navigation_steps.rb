fbc1eec8dccc145f34471cad740283e174ff56d3
Daniel Vartanov <dan@vartanov.net>
2012-10-07 09:08:11 +0600

    Scenarios for creating a post

--- features/step_definitions/navigation_steps.rb
+++ features/step_definitions/navigation_steps.rb
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+Given /я нахожусь на главной странице/ do
+  visit '/'
+end
+
 Given /я нахожусь на странице регистрации/ do
   visit '/signup'
 end
@@ -12,6 +16,14 @@ When /нажимаю "(.*)"$/ do |button|
   click_button(button)
 end
 
+When /перехожу по ссылке "(.*)"$/ do |link|
+  click_link(link)
+end
+
 Then /должен увидеть "(.*)"$/ do |text|
   page.should have_content(text)
+end
+
+Then /не должен видеть "(.*)"/ do |text|
+  page.should have_no_content(text)
 end
