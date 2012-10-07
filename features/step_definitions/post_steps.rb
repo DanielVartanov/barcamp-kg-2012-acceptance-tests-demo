56bfe48cba3cbc61f8ece70905a5870c4c222f40
Daniel Vartanov <dan@vartanov.net>
2012-10-07 11:45:41 +0600

    All green

--- features/step_definitions/post_steps.rb
+++ features/step_definitions/post_steps.rb
@@ -3,6 +3,6 @@
 When /я создаю пост с названием "(.*)"/ do |title|
   visit '/posts/new'
   fill_in "Заголовок", :with => title
-  fill_in "Текст", :with => "Ололо-шмололо"
+  fill_in "Текст", :with => "Меня трудно найти, легко потерять и невозможно забыть"
   click_button "Создать"
 end
